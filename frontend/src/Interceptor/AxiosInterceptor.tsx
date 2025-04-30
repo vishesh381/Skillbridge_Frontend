f3df52a03c4c2d3ccc5fa41d982690f8ccefc19c
Vishesh <vishesh98sharma@gmail.com>
2025-04-29 21:37:36 -0400

    added env file

--- frontend/src/Interceptor/AxiosInterceptor.tsx
+++ frontend/src/Interceptor/AxiosInterceptor.tsx
@@ -3,7 +3,7 @@ import { removeUser } from "../Slices/UserSlice";
 import { removeJwt } from "../Slices/JwtSlice";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_API_BASE_URL
 });
 
 axiosInstance.interceptors.request.use(
--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const base_url = "http://localhost:8080/auth/"
+const base_url = `${process.env.REACT_APP_API_BASE_URL}/auth/`;
 const loginUser = async (login:any)=> {
     return axios.post(`${base_url}login`, login)
         .then((result:any) => result.data)
